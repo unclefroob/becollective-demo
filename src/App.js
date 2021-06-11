4cf3666c73db0794f8c56cb49c3c8beb6e415665
unclefroob <38415462+unclefroob@users.noreply.github.com>
2021-06-11 17:10:24 +1200

    add logic to listitem to differentiate file and folder

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import InfoDisplay from "./components/InfoDisplay";
 import { fetchData, findFiles } from "./utils";
 
 const App = () => {
+  const [loaded, changeLoaded] = useState(false);
   const [myData, changeData] = useState([]);
   const [fileAmount, changeFileAmount] = useState(0);
   const [fileSize, changeFileSize] = useState(0);
@@ -13,21 +14,26 @@ const App = () => {
       const parsedFiles = findFiles(res);
       changeFileAmount(parsedFiles.files);
       changeFileSize(parsedFiles.fileSize);
+      changeLoaded(true);
     });
   }, []);
-  return (
-    <div>
-      {myData.map((item, i) => (
-        <ListItem
-          key={i}
-          data={item}
-          changeFileAmount={changeFileAmount}
-          changeFileSize={changeFileSize}
-        />
-      ))}
-      <InfoDisplay fileAmount={fileAmount} fileSize={fileSize} />
-    </div>
-  );
+  if (loaded) {
+    return (
+      <div>
+        {myData.map((item, i) => (
+          <ListItem
+            key={i}
+            data={item}
+            changeFileAmount={changeFileAmount}
+            changeFileSize={changeFileSize}
+          />
+        ))}
+        <InfoDisplay fileAmount={fileAmount} fileSize={fileSize} />
+      </div>
+    );
+  } else {
+    return <div></div>;
+  }
 };
 
 export default App;
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -6,7 +6,9 @@ import {
   faFolderOpen,
   faAngleRight,
   faAngleDown,
+  faFileAlt,
 } from "@fortawesome/free-solid-svg-icons";
+import { styleFileSize } from "../utils";
 
 const ListItem = props => {
   const { data } = props;
@@ -20,26 +22,44 @@ const ListItem = props => {
   ) : (
     <></>
   );
+  if (data.type === "folder") {
+    return (
+      <div>
+        <div onClick={handleClick} className="list-item-container">
+          <span className="list-item-icon-container">
+            <FontAwesomeIcon
+              icon={open ? faAngleDown : faAngleRight}
+              className="list-item-arrow"
+            />
 
-  return (
-    <div>
-      <div onClick={handleClick} className="list-item-container">
-        <span className="list-item-icon-container">
-          <FontAwesomeIcon
-            icon={open ? faAngleDown : faAngleRight}
-            className="list-item-arrow"
-          />
-
-          <FontAwesomeIcon
-            icon={open ? faFolderOpen : faFolder}
-            className="list-item-folder"
-          />
-        </span>
-        <span className="list-item-title">{data.name}</span>
+            <FontAwesomeIcon
+              icon={open ? faFolderOpen : faFolder}
+              className="list-item-folder"
+            />
+          </span>
+          <span className="list-item-title">{data.name}</span>
+        </div>
+        <div className="list-children">{open ? children : <></>}</div>
       </div>
-      <div className="list-children">{open ? children : <></>}</div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div>
+        <div onClick={handleClick} className="list-item-container">
+          <span className="list-item-icon-container">
+            <FontAwesomeIcon
+              icon={faFileAlt}
+              className="list-item-folder"
+              style={{ marginLeft: 21 }}
+            />
+          </span>
+          <span className="list-item-title">{data.name}</span>
+          <span className="list-item-size">{styleFileSize(data.size)}</span>
+        </div>
+        <div className="list-children">{open ? children : <></>}</div>
+      </div>
+    );
+  }
 };
 
 export default ListItem;
